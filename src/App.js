d2f301040dbae1f31e92ba30e881041e5cb4eb21
PaulFischerlaender <79645112+PaulFischerlaender@users.noreply.github.com>
2021-11-08 12:13:14 +0100

    v.0.0.2
    - Getting ready for Modal overview
    - Tried fixing Filter System

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import Switches from './components/Switches'
 import Grid from '@material-ui/core/Grid'
 
 class App extends React.Component {
+	
 	state = {
 		booksData: []
 	}
+
+
 	
 	componentDidMount() {
 		fetch('https://api.airtable.com/v0/appZDJBRdkG0hd1FU/switches?api_key=keyrEGBPInadSIyJC')
@@ -17,18 +20,20 @@ class App extends React.Component {
 			.catch(error => console.log(error))
 	}
 
+
 	/*
 	*
 	* Um zb. nach tactile zu filtern: https://api.airtable.com/v0/appZDJBRdkG0hd1FU/switches?filterByFormula=%7Btype%7D%3D%22Tactile%22&api_key=keyrEGBPInadSIyJC
 	* Sprich der Link+'?filterbyFormula='+'%7BCATEGORY%7D%3D%22VALUE%22+'&api_key=API_KEY'
 	*/
 
+
 	
 	render() {
 		const {booksData} = this.state
-		const typel = document.getElementsByName('tpye-linear');
-		if(typel.checked == true) {
-				
+		var linear = document.getElementById('type-linear')
+		if(linear.checked) {
+			alert('checked');	
 				fetch('https://api.airtable.com/v0/appZDJBRdkG0hd1FU/switches?filterByFormula=%7Btype%7D%3D%22Linear%22&api_key=keyrEGBPInadSIyJC')
 					.then(res => res.json())
 					.then(res => {
